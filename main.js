1c7996ebf44e2d69d2c13e0fb7c4f97689292085
jinwkim <jinwkim18@gmail.com>
2022-03-14 01:23:33 -0400

    Made image responsive with img-fluid

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
+window.dataLayer = window.dataLayer || [];
+function gtag() {
+    dataLayer.push(arguments);
+}
+gtag('js', new Date());
+gtag('config', 'G-ML0DD8HX2L');
+
 var shiftWindow = function() { scrollBy(0, -70) };
 window.addEventListener("hashchange", shiftWindow);
 function load() { if (window.location.hash) shiftWindow(); }
