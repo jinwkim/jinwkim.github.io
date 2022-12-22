7786514ebd6fee6bc0885823492bc6f64ed363b4
jinwkim <jinwkim18@gmail.com>
2022-12-22 16:52:12 +0900

    Add Jitsu for tracking

---
+++ analytics.js
@@ -0,0 +1,110 @@
+// Track home page upon visit
+mixpanel.track('Home page');
+console.log("Home page");
+
+function mainLogo(){
+    mixpanel.track("JinKim Logo");
+    jitsu('track', 'JinKim Logo');
+    console.log("JinKim Logo");
+}
+
+// Navbar
+function navbar() {
+    mixpanel.track("Navbar");
+    jitsu('track', 'Navbar');
+    console.log("Navbar");
+}
+
+function about() {
+    mixpanel.track("About");
+    jitsu('track', 'About');
+    console.log("About");
+}
+
+function experiences() {
+    mixpanel.track("Experiences");
+    jitsu('track', 'Experiences');
+    console.log("Experiences");
+}
+
+function portfolio() {
+    mixpanel.track("Portfolio");
+    jitsu('track', 'Portfolio');
+    console.log("Portfolio");
+}
+
+function contact() {
+    mixpanel.track("Contact");
+    jitsu('track', 'Contact');
+    console.log("Contact");
+}
+
+// About Me
+function foodblog(){
+    mixpanel.track("Instagram Food Blog");
+    jitsu('track', 'Instagram Food Blog');
+    console.log("Instagram Food Blog");
+}
+
+// Experiences
+function intersystems(){
+    mixpanel.track("InterSystems");
+    jitsu('track', 'InterSystems');
+    console.log("InterSystems");
+}
+
+function mit(){
+    mixpanel.track("MIT");
+    jitsu('track', 'MIT');
+    console.log("MIT");
+}
+
+function archery(){
+    mixpanel.track("Archery");
+    jitsu('track', 'Archery');
+    console.log("Archery");
+}
+
+// Portfolio
+function emotionDetection(){
+    mixpanel.track("Emotion Detection");
+    jitsu('track', 'Emotion Detection');
+    console.log("Emotion Detection");
+}
+
+function instagramAutomation(){
+    mixpanel.track("Instagram Automation");
+    jitsu('track', 'Instagram Automation');
+    console.log("Instagram Automation");
+}
+
+function alexaRestaurant(){
+    mixpanel.track("Alexa Restaurant Finder");
+    jitsu('track', 'Alexa Restaurant Finder');
+    console.log("Alexa Restaurant Finder");
+}
+
+function cardGame(){
+    mixpanel.track("Card Game App");
+    jitsu('track', 'Card Game App');
+    console.log("Card Game App");
+}
+
+function personalWebsite(){
+    mixpanel.track("Personal Website");
+    jitsu('track', 'Personal Website');
+    console.log("Personal Website");
+}
+
+// Contact
+function github() {
+    mixpanel.track("GitHub");
+    jitsu('track', 'GitHub');
+    console.log("GitHub");
+}
+
+function linkedin() {
+    mixpanel.track("LinkedIn");
+    jitsu('track', 'LinkedIn');
+    console.log("LinkedIn");
+}
